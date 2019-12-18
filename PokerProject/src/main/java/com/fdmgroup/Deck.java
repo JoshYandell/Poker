e5cbc5fb9b31a3d8b6b9630b995f641a70d1bead
Joshua Yandell <Joshua.Yandell@fdmgroup.com>
2019-12-18 11:38:45 -0500

    pushing finished deck, hand, cardfactory

--- PokerProject/src/main/java/com/fdmgroup/Deck.java
+++ PokerProject/src/main/java/com/fdmgroup/Deck.java
@@ -1,9 +1,33 @@
 package com.fdmgroup;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class Deck {
-
-
-	List<Card> deck;
+	
+	private static Deck s_deck=new Deck();
+	
+	private CardFactory cardFactory = new CardFactory();
+	private List<Card> deck = new ArrayList<>();
+	
+	public static Deck getDeck() {
+		return s_deck;
+	}
+	
+	public Card popCard() {
+		return deck.remove(0);
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(deck);
+	}
+	
+	public void init() {
+		for(Suit s : Suit.values()) {
+			for(int i=1; i<14; i++) {
+				deck.add(cardFactory.createCard(i, s));
+			}
+		}
+	}
 }
